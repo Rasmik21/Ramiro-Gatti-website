47466e6e564c7f88034a65c175a645f1f5db67ce
Rami <ramirogatti21@gmail.com>
2022-04-05 06:09:52 -0300

    version actualizada: html + css + SASS + JS

--- main.js
+++ main.js
@@ -11,14 +11,22 @@ console.log("Hola " + nombreIngresado + ", bienvenido/a!");
 
 
 
-let serieFavorita = prompt ("Cuál es tu serie favorita?")
+let animeFavorito = prompt ("Cuál es tu serie favorita?")
 
- if (serieFavorita == "Naruto"){
+if (animeFavorito == "Naruto"){
 
     alert ("El mio también!");
-} else if (serieFavorita == "One Piece"){
-    alert ("Ese anime no me gusta");
-} else {
+} 
+else if (animeFavorito == "Bleach"){
+    alert ("Me encanta! Mi personaje favorito es Rukia.");
+}
+else if (animeFavorito == "One Piece"){
+    alert ("Ese anime no lo vi, es muy largo.");
+}
+else if (animeFavorito == "Dragon Ball"){
+    alert ("Entonces podemos ser amigos.");
+}
+else {
     //string template
     alert (`Tu anime favorito es ${serieFavorita}. El mío es Dragon Ball Z.`);
 }
